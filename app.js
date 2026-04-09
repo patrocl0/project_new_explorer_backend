e47390ca6afd3b2dda05909b6e6a4d342178a226
Brayan Mora <brayanestiven1208@gmail.com>
2026-04-09 16:38:00 -0500

    Se realizan ajuste en los cors allowOrigin se permitan origenes y se elimina el middleware de los cors antes era manuel ahora queda en manos de express

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const { errors } = require("celebrate");
+const cors = require("cors");
 
 const { login, createUser } = require("./controllers/user");
 const usersRouter = require("./routes/users");
@@ -11,18 +12,29 @@ const { requestLogger } = require("./middlewares/requestLogger");
 const { errorLogger } = require("./middlewares/errorLogger");
 const errorHandler = require("./middlewares/errorHandler");
 const auth = require("./middlewares/auth");
-const corsMiddleware = require("./middlewares/cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const allowedOrigins = [
+  "https://newexplorer.ignorelist.com",
+  "https://api.newexplorer.ignorelist.com",
+  "http://localhost:5173",
+];
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB conectado"))
   .catch((err) => console.error(err));
 
-app.use(corsMiddleware);
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  }),
+);
 
 app.use(express.json());
 
